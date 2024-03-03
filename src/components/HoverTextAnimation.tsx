d949f551260259f048ebad44ced8df2e22e9e683
Sumon Chandra <topu199041@gmail.com>
2024-03-03 18:03:01 +0600

    animation: Text hover animation
    This is the text hover animation. When we hover a character the charater will bold as well as it's siblings

---
+++ src/components/HoverTextAnimation.tsx
@@ -0,0 +1,21 @@
+
+const HoverTextAnimation = () => {
+   return (
+      <main className="flex min-h-screen flex-col items-center justify-center">
+         <div className="text-4xl font-light">
+            {
+               "Hello World".split("").map((child, idx) => (
+                  <span
+                     key={idx}
+                     className="hoverText text-slate-400 hover:font-black hover:text-slate-100 transition-all duration-300:"
+                  >
+                     {child}
+                  </span>
+               ))
+            }
+         </div>
+      </main>
+   )
+}
+
+export default HoverTextAnimation
